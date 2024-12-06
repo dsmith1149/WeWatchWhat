81abcec0b32077ee55e153357628b709a7664852
dsmith1149 <dantayvionsmith@gmail.com>
2024-12-06 17:16:26 -0600

    push

---
+++ src/main/java/org/launchcode/BingeBuddy/BingeBuddyApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.BingeBuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BingeBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BingeBuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/BingeBuddy/BingeBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.BingeBuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BingeBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
