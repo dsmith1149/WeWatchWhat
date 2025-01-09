2cf22b9bbbcf5c07b4b750c9336ea30a52144461
cfilla371 <64501150+cfilla371@users.noreply.github.com>
2025-01-09 12:57:51 -0600

    still figuring out the web API

--- bingebuddy/src/components/SearchMovieComponent.jsx
+++ bingebuddy/src/components/SearchMovieComponent.jsx
@@ -1,22 +1,62 @@
+import { useState } from "react";
 
-const SearchMovieComponent = () => {
-  return (
+export default function SearchMovie() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  }
+ 
+  const handleSubmit = (event) => {
+    let input = searchTerm;
+    alert(1+input)
+    let url = `https://www.omdbapi.com/?s=${input}&apikey=a6836b7c`
+    alert(2 + url)
+    const fetchPromise = fetch(url);
+    fetchPromise.then(function(response){
+      alert(3+response)
+    })
+    }
+  
+  return(
     <div>
-        <h1 className='center'>             
-            Search for a Movie!
-        </h1>
-        <form>
-          <label>
-          Search:
-          <input type="text" name="movieName"/>
-          </label>
-          <input type="submit" value="Submit"/>
-        </form>
-        <script src="search.js"/>
-          
+      <h1>Enter the name of a movie!</h1>
+      <form onSubmit={handleSubmit}>
         
-      </div>
+          <input type="text" value={searchTerm} onChange={handleSearch}/>
+          <input type="submit"/>
+        <b id="movie-data"/>
+        
+    
+      </form>
+    </div>
   )
 }
 
-export default SearchMovieComponent
+
+
+// import {useState} from 'react';
+// export default function MovieSearch() {
+//   const [searchTerm, setSearchTerm] =useState;
+
+// const SearchMovieComponent = () => {
+//   return (
+//     <div>
+//         <h1 className='center'>             
+//             Search for a Movie!
+//         </h1>
+//         <form>
+//           <label>
+//           Search:
+//           <input type="text" name="movieName"/>
+//           </label>
+//           <button>search</button>
+//         </form>
+        
+//         <script src="search.js"/>
+          
+        
+//       </div>
+//   )
+// }}
+
+// export default SearchMovieComponent
