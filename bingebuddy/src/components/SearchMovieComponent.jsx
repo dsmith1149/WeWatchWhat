db43d65d9b7075c36990169e444ab3a8b5fa64ac
cfilla371 <64501150+cfilla371@users.noreply.github.com>
2025-01-14 15:06:01 -0600

    still working on displaying serch results

--- bingebuddy/src/components/SearchMovieComponent.jsx
+++ bingebuddy/src/components/SearchMovieComponent.jsx
@@ -1,62 +1,53 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
 
 export default function SearchMovie() {
+  const results = [];
   const [searchTerm, setSearchTerm] = useState("");
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
+    
   }
- 
-  const handleSubmit = (event) => {
-    let input = searchTerm;
-    alert(1+input)
-    let url = `https://www.omdbapi.com/?s=${input}&apikey=a6836b7c`
-    alert(2 + url)
-    const fetchPromise = fetch(url);
-    fetchPromise.then(function(response){
-      alert(3+response)
-    })
+    // const handleSubmit = () => {
+    //   let title = ""
+    //   fetch(`https://www.omdbapi.com/?s=${searchTerm}&apikey=a6836b7c`).then(function(response){
+    //     response.json().then(function(json){
+    //       console.log(json)
+    //       for (let list in json.Search){
+    //          title = json["Search"][list]["Title"]
+    //          results.push(title)
+    //          console.log(results)
+             
+    //       }
+    //     })
+    //     })
+        
+    //   }
+      
+  const handleSubmit = () => {
+    const fetchData = async () => {
+      const result = await fetch(`https://www.omdbapi.com/?s=${searchTerm}&apikey=a6836b7c`)
+      result.json().then(json => {
+        for(item in json["Search"]){
+          result.push(json["Search"][item]["Title"])
+        }
+      })
     }
-  
+    fetchData();
+  }
+
   return(
     <div>
       <h1>Enter the name of a movie!</h1>
-      <form onSubmit={handleSubmit}>
-        
+      <b>
           <input type="text" value={searchTerm} onChange={handleSearch}/>
-          <input type="submit"/>
-        <b id="movie-data"/>
-        
-    
-      </form>
+          <input type="submit" onClick={handleSubmit}/>
+      </b>
+      <ol>
+
+      </ol>
+      
+     
     </div>
   )
 }
-
-
-
-// import {useState} from 'react';
-// export default function MovieSearch() {
-//   const [searchTerm, setSearchTerm] =useState;
-
-// const SearchMovieComponent = () => {
-//   return (
-//     <div>
-//         <h1 className='center'>             
-//             Search for a Movie!
-//         </h1>
-//         <form>
-//           <label>
-//           Search:
-//           <input type="text" name="movieName"/>
-//           </label>
-//           <button>search</button>
-//         </form>
-        
-//         <script src="search.js"/>
-          
-        
-//       </div>
-//   )
-// }}
-
-// export default SearchMovieComponent
