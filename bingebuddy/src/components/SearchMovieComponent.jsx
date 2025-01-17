0265b2e5d9ed06a1d269f5a33235931f54f3663e
cfilla371 <64501150+cfilla371@users.noreply.github.com>
2025-01-17 13:18:36 -0600

    Layout changes. cleaned up code Child key issue remains

--- bingebuddy/src/components/SearchMovieComponent.jsx
+++ bingebuddy/src/components/SearchMovieComponent.jsx
@@ -1,41 +1,39 @@
-import {useEffect, useState } from "react";
-
+import {useState } from "react";
 
 export default function SearchMovie() {
-  let tMovies = []
-  let sMovies = []
-  let initial = []
-  let link = ""
-  let title = ""
-  let poster = ""
+  let tMovies = [];
+  let sMovies = [];
+  let initial = [];
+  let title = "";
+  let poster = "";
+  let movieID = "";
+
   const [searchTerm, setSearchTerm] = useState("");
   const[showMovies, setShowMovies] = useState(initial)
+
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
-    
   }
 
   const displayMovies = function(movie){
-     
-
-     
     return <ul><ul>{movie[0]}</ul> 
-    <img src={movie[1]}></img></ul>
+    <a href={movie[2]}><img src={movie[1]}></img></a></ul>
   }
      
   const handleSubmit = () => {
-    sMovies=[]; //move this somewhere
     const fetchData = async () => {
       const result = await fetch(`https://www.omdbapi.com/?s=${searchTerm}&apikey=a6836b7c`)
       result.json().then(json => {
         for(let item in json["Search"]){
           title = json["Search"][item]["Title"]
           poster = json["Search"][item]["Poster"]
-          tMovies = [title, poster]
+          movieID = json["Search"][item]["imdbID"]
+          tMovies = [title, poster, movieID]
           sMovies.push(tMovies)
         }
         initial = sMovies
         setShowMovies(initial)
+        sMovies=[];
       })
     }
     fetchData()
@@ -49,7 +47,6 @@ export default function SearchMovie() {
           <input type="submit" onClick={handleSubmit}/>
       </b>
       <ul>{showMovies.map(displayMovies)}</ul>
-      
     </div>
   )
 }
